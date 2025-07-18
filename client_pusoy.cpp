c408e3ec9fb9a998f4f38d1931b1d78746f68a82
dennc15 <149035102+dennc15@users.noreply.github.com>
2025-07-19 05:26:09 +0800

    final client file

--- client_pusoy.cpp
+++ client_pusoy.cpp
@@ -1,55 +1,76 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<unistd.h>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
 using namespace std;
 
-int main()
+const int PORT = 8080;
+
+int main() 
 {
-    int playerID;
+    int sckt = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
     
-    cout << "Enter your player ID (1-3): ";
-    cin >> playerID;
-    
-    if (playerID < 1 || playerID > 3){
-        cerr << "Invalid player ID. Please enter from 1-3 only.\n";
-        return 1;
+    //socket
+    if ((sckt = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        cerr << "Couldn't create socket." << endl;
+        return -1;
     }
     
-    string inPipe = "client" + to_string(playerID) + "_in";
-    string outPipe = "client" + to_string(playerID) + "_out";
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
     
-    //open pipes
-    ifstream fromServer(outPipe);
-    oftream toServer(inPipe);
-    
-    if (!fromServer.is_open() || !toServer.is_open())
+    //converting IPs from text to binary
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
     {
-        cerr << "Failed to open one or both pipes. \n";
-        cerr << "Make sure the server created the FIFOs.\n";
-        return 1;
+        cerr << "Invalid address" << endl;
+        return -1;
     }
     
-    cout << "Connected to Pusoy Clash as Player " << playerID << "!\n";
-    
-    string line;
-    while (getline(fromServer, line))
+    //connection to server
+    if (connect(sckt, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        cout << line << endl;
-        
-        if (line.find("Your choice: ") != string:npos)
+        cerr << "Failed to connect." << endl;
+        return -1;
+    }
+    cout << "Successfully connected to server!\n";
+    
+    while(true) 
+    {
+        memset(buffer, 0, sizeof(buffer));
+        int valread = read(sckt, buffer, sizeof(buffer));
+        if (valread <= 0)
         {
-            int choice;
-            while (!(cin >> choice) || (choice != 1 && choice != 2))
-            {
-                cin.clear();
-                cin.ignore(1000, '\n');
-                cout << "Error. Please choose between 1 and 2 only: ";
-            }
-            toServer << choice << endl;
-            toServer.flush();
+            cout << "[Disconnected from server]\n";
+            break;
+        }
+        string msg(buffer);
+        msg.erase(msg.find_last_not_of("\r\n") + 1);
+        
+        cout << msg << endl;
+        
+        if (msg.find("ENTER_NAME") != string::npos)
+        {
+            cout << "Enter your player name: ";
+            string playerID;
+            getline (cin, playerID);
+            send (sckt, playerID.c_str(), playerID.length(), 0);
+        }
+        
+        if (msg.find("YOUR_TURN") != string::npos)
+        {
+            cout << "Draw a card\n2. Pass\n";
+            cout << "Your choice: ";
+            string choice;
+            getline(cin, choice);
+            send(sckt, choice.c_str(), choice.length(), 0);
         }
     }
-    cout << "\n[Disconnected from the server]\n";
+    close(sckt);
     return 0;
 }
